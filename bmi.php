765c5a32ab393686e368bea1e70d6c3472e2fae1
hee61 <31370094+hee61@users.noreply.github.com>
2020-03-30 19:52:30 -0500

    Update bmi.php

--- bmi.php
+++ bmi.php
@@ -13,19 +13,35 @@
 	echo 'we have made it this far';
 	$bmi_button = $_POST['bmiButton'];
 	$feet = $_POST['feet'];
+		
 	$inches = $_POST['inches'];
 	$weight_lbs = $_POST['Weight'];
-	
+	$status;
 	$total_inches = $inches + ($feet * 12);
 	
 	$weight_kg = $weight_lbs * .45;
 	$meters = $total_inches * .025;
 	$meters_squared = pow($meters, 2);
 	$bmi = $weight_kg/$meters_squared;
-	echo $bmi;
+	if ($bmi < 18.5)
+	{
+		$status = "Underweight";
+	}
+	else if ($bmi < 24.9)
+	{
+		$status = "Normal";
+	}
+	else if ($bmi < 30.0)
+	{
+		$status = "Normal";
+	}
+	else 
+	{
+		$status = "Obese";
+	}
 	?>
 	
-	
+		<h1>Your bmi is </h1><?php echo $bmi;?><h1>This is considered</h1><?php echo $status;?>
 
 	</body>
 </html>
